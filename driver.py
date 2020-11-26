4dc6e3c9acf224d19a01ad6d2aba28a2a6b40f25
Anshuman Dash <anshuman@ucsb.edu>
2020-11-26 15:43:42 -0800

    added class support for driver

--- driver.py
+++ driver.py
@@ -3,49 +3,49 @@ import RPi.GPIO as GPIO
 
 class Driver():
     
-    def __init__(pins):
+    def __init__(self, pins):
         #Right motor: pins[0]
         #Right back : pins[1]
         #Left motor: pins[2]
         #Left back : pins[3]
         self.pins = pins
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(pins[0], GPIO.OUT)
-        GPIO.setup(pins[1], GPIO.OUT)
-        GPIO.setup(pins[2], GPIO.OUT)
-        GPIO.setup(pins[3], GPIO.OUT)
+        GPIO.setup(self.pins[0], GPIO.OUT)
+        GPIO.setup(self.pins[1], GPIO.OUT)
+        GPIO.setup(self.pins[2], GPIO.OUT)
+        GPIO.setup(self.pins[3], GPIO.OUT)
 
-    def forwards():
-        GPIO.output(pins[0],0)
-        GPIO.output(pins[1],1) 
-        GPIO.output(pins[2],0) 
-        GPIO.output(pins[3],1)
+    def forwards(self):
+        GPIO.output(self.pins[0],0)
+        GPIO.output(self.pins[1],1) 
+        GPIO.output(self.pins[2],0) 
+        GPIO.output(self.pins[3],1)
         
-    def backwards():
-        GPIO.output(pins[0],1)
-        GPIO.output(pins[1],0) 
-        GPIO.output(pins[2],1) 
-        GPIO.output(pins[3],0)
+    def backwards(self):
+        GPIO.output(self.pins[0],1)
+        GPIO.output(self.pins[1],0) 
+        GPIO.output(self.pins[2],1) 
+        GPIO.output(self.pins[3],0)
     
-    def left():
-        GPIO.output(pins[0],1)
-        GPIO.output(pins[1],0) 
-        GPIO.output(pins[2],0) 
-        GPIO.output(pins[3],1)
+    def left(self):
+        GPIO.output(self.pins[0],1)
+        GPIO.output(self.pins[1],0) 
+        GPIO.output(self.pins[2],0) 
+        GPIO.output(self.pins[3],1)
     
-    def right():
-        GPIO.output(pins[0],0)
-        GPIO.output(pins[1],1) 
-        GPIO.output(pins[2],1) 
-        GPIO.output(pins[3],0)
+    def right(self):
+        GPIO.output(self.pins[0],0)
+        GPIO.output(self.pins[1],1) 
+        GPIO.output(self.pins[2],1) 
+        GPIO.output(self.pins[3],0)
         
-    def stop():
-        GPIO.output(pins[0],0)
-        GPIO.output(pins[1],0) 
-        GPIO.output(pins[2],0) 
-        GPIO.output(pins[3],0)
+    def stop(self):
+        GPIO.output(self.pins[0],0)
+        GPIO.output(self.pins[1],0) 
+        GPIO.output(self.pins[2],0) 
+        GPIO.output(self.pins[3],0)
         
-    def kill():
+    def kill(self):
         stop()
         GPIO.cleanup()
     
